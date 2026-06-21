2bddbd7f31cbebbe7f98b08e89769f0cb7787321
Mudasir Ali <unarmudasir@gmail.com>
2026-06-21 22:04:23 +0500

    Bump version code to 3 and update version name to 2.1.1

--- AndroidApp/ApplyTrack/app/build.gradle.kts
+++ AndroidApp/ApplyTrack/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.applytrack"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "2.0.0"
+    versionCode = 3
+    versionName = "2.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
