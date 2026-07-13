bce28a864efca90919b523c589a71e31d3f73eeb
Mudasir Ali <unarmudasir@gmail.com>
2026-07-13 19:09:17 +0500

    chore: bump application version to 3.0.1

--- AndroidApp/ApplyTrack/app/build.gradle.kts
+++ AndroidApp/ApplyTrack/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.applytrack"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "3.0.0"
+    versionCode = 5
+    versionName = "3.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
